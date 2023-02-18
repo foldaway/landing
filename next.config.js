99b1f116142e6b6985b5184522309cc4293110b7
Chester How <chesterhow@gmail.com>
2023-02-18 20:12:17 +0800

    chore: fresh nextjs project

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
-module.exports = {
-  target: 'serverless',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack', 'url-loader'],
-    });
-
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: 'raw-loader',
-    });
-
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      type: 'json', // Required by Webpack v4
-      use: 'yaml-loader',
-    });
-
-    config.resolve.fallback = {
-      fs: false,
-      path: require.resolve('path-browserify'),
-    };
-
-    return config;
-  },
-
-  env: {
-    URL: process.env.URL,
-    GITHUB_API_TOKEN: process.env.GITHUB_API_TOKEN,
-  },
-};
+module.exports = nextConfig
--- src/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import {} from 'styled-components';
-
-import { Theme as ThemeType } from './styles/theme';
-
-declare module 'styled-components' {
-  type Theme = ThemeType;
-  export interface DefaultTheme extends Theme {}
-}
